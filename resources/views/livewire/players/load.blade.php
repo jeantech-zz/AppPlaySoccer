339ae7a476fa6cd0eabe6fb2cc4aca2559a4ba3d
Jennifer <jeante05@gmail.com>
2021-11-18 05:25:32 -0500

    load players

---
+++ resources/views/livewire/players/load.blade.php
@@ -0,0 +1,24 @@
+<!-- Modal -->
+<div wire:ignore.self class="modal fade" id="modalLoad" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Create New Team</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                     <span aria-hidden="true close-btn">×</span>
+                </button>
+            </div>
+           <div class="modal-body">
+           <form wire:submit.prevent="save"
+           enctype="multipart/form-data"
+           >
+            <input type="file" wire:model="load">  @error('photo') <span class="error">{{ $message }}</span> @enderror
+            </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary close-btn" data-dismiss="modal">Close</button>
+                <button type="button" wire:click.prevent="load()" class="btn btn-primary close-modal">Load</button>
+            </div>
+        </div>
+    </div>
+</div>
